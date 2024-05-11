ec6aded9388a9a4e9516b2b13956a5fdb0b14f23
bllsglm <bilalsaglam92@gmail.com>
2024-05-11 12:47:15 +0300

    ready production

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000': 'https://goalsetter-bn6g.onrender.com'
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000': 'https://pfactory.onrender.com/login'
 export const USERS_URL ='/api/users';
 export const GOALS_URL ='/api/goals';
 export const UPLOAD_URL ='/api/upload'; 
